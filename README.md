4fab1b8b5239f418bd47a30e1536fe78559dcfef
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-11-13 00:32:34 -0800

    innitial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# @lacolaco/ngx-web-bundles
+
+# DEEPLY EXPERIMENTAL TOOL! BE CAREFUL TO USE!
+
+Angular CLI builder for generate Web Bundle file (.wbn). 
+To learn about Web Bundles, read https://web.dev/web-bundles.
+
+This tool is created for just trying new (experimental) web proposal with Angular CLI. 
+**PLEASE DON'T RELY ON THIS ANYWAY!!**
+
+## Usage
+
+See the `testapp` directory for example.
+
+### Install the builder
+
+```
+$ yarn add -D @lacolaco/ngx-web-bundles
+```
+
+### Add the architect command to your `angular.json`.
+
+- `options.browserTarget`: Identify target configuration for building app. 
+- `options.primaryURL`: Tell the primary URL to the Web Bundle. (used by browser)
+
+```
+...
+      "architect": {
+        "gen-bundle": {
+          "builder": "@lacolaco/ngx-web-bundles:generate:production",
+          "options": {
+            "browserTarget": "testapp:build",
+            "primaryURL": "https://example.com/"
+          }
+        },
+...      
+```
+
+### Run it
+
+Execute the command;
+
+```
+$ ng run testapp:gen-bundle
+```
+
+
---
+++ index.ts
@@ -0,0 +1,77 @@
+import {
+  BuilderContext,
+  createBuilder,
+  targetFromTargetString
+} from "@angular-devkit/architect";
+import {
+  BrowserBuilderOptions,
+  executeBrowserBuilder
+} from "@angular-devkit/build-angular";
+import { JsonObject } from "@angular-devkit/core";
+import * as fs from "fs";
+import * as glob from "glob";
+import * as mime from "mime";
+import * as path from "path";
+import * as rimraf from "rimraf";
+import { BundleBuilder } from "wbn";
+
+interface Options extends JsonObject {
+  browserTarget: string;
+  primaryURL: string;
+  outputPath: string | null;
+}
+
+async function generateWebBundle(
+  distDir: string,
+  primaryURL: string,
+  output: string
+) {
+  primaryURL = primaryURL.endsWith("/") ? primaryURL : primaryURL + "/";
+
+  const wbnBuilder = new BundleBuilder(primaryURL);
+  glob.sync(`${distDir}/*`).forEach(file => {
+    const requestURL = primaryURL + path.relative(distDir, file);
+    wbnBuilder.addExchange(
+      requestURL,
+      200,
+      { "Content-Type": mime.getType(file) || "text/plain" },
+      fs.readFileSync(file)
+    );
+  });
+  wbnBuilder.addExchange(
+    primaryURL,
+    200,
+    { "Content-Type": "text/html" },
+    fs.readFileSync(`${distDir}/index.html`)
+  );
+  fs.writeFileSync(path.resolve(distDir, output), wbnBuilder.createBundle());
+  return;
+}
+
+async function build(options: Options, context: BuilderContext) {
+  const browserTarget = targetFromTargetString(options.browserTarget);
+  const browserTargetOptions = ((await context.getTargetOptions(
+    browserTarget
+  )) as unknown) as BrowserBuilderOptions;
+
+  context.logger.info(`Initializing.`);
+  rimraf.sync(browserTargetOptions.outputPath);
+  context.logger.info(`Building app.`);
+  const { outputPath } = await executeBrowserBuilder(
+    browserTargetOptions,
+    context
+  ).toPromise();
+
+  context.logger.info(`Generating Web Bundle.`);
+  const wbnOutputPath =
+    options.outputPath || path.resolve(outputPath, "out.wbn");
+  await generateWebBundle(outputPath, options.primaryURL, wbnOutputPath);
+
+  context.logger.info(
+    `Web Bundle file has been generated at ${wbnOutputPath} .`
+  );
+  context.reportStatus(`Done.`);
+  return { success: true };
+}
+
+export default createBuilder(build);
